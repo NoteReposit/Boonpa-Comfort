92f1d8adf6886a02e237d6b52509cbddf9213f0b
NoteReposit <noteyosakon@gmail.com>
2025-02-24 23:05:59 +0700

    first commit new generation

---
+++ database/migrations/2025_02_24_100153_update_users_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('first_name', 50)->after('id');
+            $table->string('last_name', 50)->after('first_name');
+            $table->string('address')->nullable()->after('password');
+            $table->string('phone', 15)->nullable()->after('address');
+            $table->enum('role', ['customer', 'employee'])->default('customer')->after('phone');
+            $table->string('image_url')->nullable();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['first_name', 'last_name', 'user_name', 'address', 'phone', 'role']);
+        });
+    }
+};
